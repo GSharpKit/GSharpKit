d308b443662a4c25e8da8b472bc8c6007591381f
Mikkel Kruse Johnsen <mikkel@xmedicus.com>
2022-02-08 14:35:28 +0100

    macOS pkg scripts

--- make-pkg-install.sh
+++ make-pkg-install.sh
@@ -12,16 +12,12 @@ mkdir -p $TO/etc
 cp -r $FROM/etc/dbus-1 $TO/etc/
 cp -r $FROM/etc/fonts $TO/etc/
 cp -r $FROM/etc/gtk-3.0 $TO/etc/
-cp -r $FROM/etc/mono $TO/etc/
 cp -r $FROM/etc/pki $TO/etc/
 cp ca-bundle.crt $TO/etc/pki/
 #cp -r $FROM/etc/sane.d $TO/etc/
 
 # BIN directory
 mkdir -p $TO/bin
-cp $FROM/bin/mono $TO/bin/
-cp $FROM/lib/mono/4.5/cert-sync.exe $TO/bin/
-
 cp $FROM/bin/dbus-daemon $TO/bin/
 cp $FROM/bin/dbus-launch $TO/bin/
 cp $FROM/bin/dbus-send $TO/bin/
@@ -35,21 +31,12 @@ cp $FROM/bin/fc-cache $TO/bin/
 # LIB directory
 mkdir -p $TO/lib
 cp $FROM/lib/*.dll $TO/lib/
-cp $FROM/lib/*.dll.config $TO/lib/
 
 # Hack to make gphoto work.
 #cp $TO/lib/libgphoto2-sharp.dll $TO/lib/libgphoto2.dll
 
 cp -a $FROM/lib/*.dylib $TO/lib/
  
-mkdir -p $TO/lib/mono/4.5
-cp -r $FROM/lib/mono/4.5/mscorlib.dll $TO/lib/mono/4.5/
-cp -r $FROM/lib/mono/4.5/Facades $TO/lib/mono/4.5/
-rm -f $TO/lib/mono/4.5/Facades/*.pdb
-cp -r $FROM/lib/mono/gac $TO/lib/mono/
-rm -f $TO/lib/mono/gac/*/*/*.pdb
-rm -f $TO/lib/mono/gac/*/*/*.mdb
-
 mkdir -p $TO/lib/enchant
 cp -r $FROM/lib/enchant/*.so $TO/lib/enchant/
 
